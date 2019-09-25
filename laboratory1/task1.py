29f755589266a88e6ff9aeb88350eff875561e09
masiranna <mashir.anna@gmail.com>
2019-09-25 18:51:30 +0300

    завдання 1

---
+++ laboratory1/task1.py
@@ -0,0 +1,16 @@
+print("Варіант 13 - число Армстронга. Машир А. М. КМ-93")
+n = int(input("Введіть число "))
+number = 1
+def armst():
+    global number
+    sum = 0
+    List = list(str(number))
+    L = len(List)
+    for i in List:
+        sum += (int(i) ** L)
+    if number == sum:
+        print(str(sum) + " є числом Армстронга")
+    number += 1
+    return number
+while number <= n:
+    armst()
